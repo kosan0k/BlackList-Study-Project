0be52946a2c0a3003c417f583c0a34eeca1d2885
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-10-17 19:32:52 +0300

    Добавьте файлы проекта.

---
+++ Sources/Ui/Wpf/BlackList.Ui.Wpf.Host/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BlackList.Ui.Wpf.Host
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
