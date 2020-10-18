1bc5d7a7cb37094acbab400101d6b6a7bf87df56
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-10-18 10:43:59 +0300

    [~] Changed BlackList.Storage.Sql project to netFramework 4.8

--- Sources/Ui/Wpf/BlackList.Ui.Wpf.Host/App.xaml.cs
+++ Sources/Ui/Wpf/BlackList.Ui.Wpf.Host/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BlackList.Ui.Wpf.Host.ViewModels;
 using System.Windows;
 
 namespace BlackList.Ui.Wpf.Host
@@ -13,5 +8,14 @@ namespace BlackList.Ui.Wpf.Host
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainView = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+
+            mainView.Show();
+        }
     }
 }
---
+++ Sources/Ui/Wpf/BlackList.Ui.Wpf.Host/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BlackList.Storage;
+using BlackList.Storage.Sql;
+using System.Configuration;
+
+namespace BlackList.Ui.Wpf.Host.ViewModels
+{
+    public class MainViewModel
+    {
+        private static IStorage _storage;
+
+        public MainViewModel() 
+        {
+            var dbConnectionString = ConfigurationManager.ConnectionStrings["BlackListDatabase"].ConnectionString;
+            _storage = new SqlStorage(dbConnectionString);
+        }
+    }
+}
