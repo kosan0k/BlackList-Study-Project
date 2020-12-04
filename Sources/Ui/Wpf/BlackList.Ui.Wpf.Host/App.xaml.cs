ae0f74ae4a12585c7df0634d358d2af00310522d
Alexander Zhishkevich <kosan0k230@gmail.com>
2020-12-04 18:20:42 +0300

    [+] IoC container implementation in app.xaml.cs

--- Sources/Ui/Wpf/BlackList.Ui.Wpf.Host/App.xaml.cs
+++ Sources/Ui/Wpf/BlackList.Ui.Wpf.Host/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using BlackList.Storage.Sql;
+﻿using BlackList.Storage;
+using BlackList.Storage.Sql;
 using BlackList.Ui.Wpf.Host.ViewModels;
+using Ninject;
 using System.Configuration;
 using System.Windows;
 
@@ -10,16 +12,27 @@ namespace BlackList.Ui.Wpf.Host
     /// </summary>
     public partial class App : Application
     {
+        private IKernel _container;
+        private string _dbConnectionString;
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            var dbConnectionString = ConfigurationManager.ConnectionStrings["BlackListDatabase"].ConnectionString;
+            _dbConnectionString = ConfigurationManager.ConnectionStrings["BlackListDatabase"].ConnectionString;
+
+            ConfigureContainer();
 
             var mainView = new MainWindow()
             {
-                DataContext = new MainViewModel(new SqlStorage(dbConnectionString))
+                DataContext = new MainViewModel(_container.Get<IStorage>())
             };
 
             mainView.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            this._container = new StandardKernel();
+            _container.Bind<IStorage>().To<SqlStorage>().InSingletonScope().WithConstructorArgument(_dbConnectionString);
         }
     }
 }
